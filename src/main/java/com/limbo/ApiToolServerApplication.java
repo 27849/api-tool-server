aff8aeb9dc35db55e9c2c391ab02d1afb3f065d5
limbo <xia27849@163.com>
2021-08-06 02:05:56 +0800

    init project

---
+++ src/main/java/com/limbo/ApiToolServerApplication.java
@@ -0,0 +1,13 @@
+package com.limbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiToolServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiToolServerApplication.class, args);
+    }
+
+}
